df76ca6abdfd04673ad62fabbb256978e8cf7109
poojaagr21 <poojaagrawal2193@gmail.com>
2025-07-31 19:47:37 -0500

    Update lambda.tf

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,5 +1,20 @@
-resource "aws_lambda_function" "upload_model" {
-  # existing code...
 
-  reserved_concurrent_executions = 5  # Optional limit
+resource "aws_lambda_function" "upload_model" {
+  function_name = "upload_model"
+  handler       = "upload_model.lambda_handler"  # your Python handler function
+  runtime       = "python3.9"
+
+  # Path to the zipped Lambda deployment package
+  filename      = "${path.module}/../lambda/function.zip"
+
+  role          = aws_iam_role.lambda_exec_role.arn
+
+  timeout       = 30      # Timeout in seconds
+  memory_size   = 128     # Memory allocation in MB
+
+  # Optional: limit concurrency to avoid throttling or overload
+  reserved_concurrent_executions = 5
+
+  # To force a new deployment when the zip changes
+  source_code_hash = filebase64sha256("${path.module}/../lambda/function.zip")
 }
